25938b75f1367aca6be7d6b8a076dbf76c98cb9d
Jamie Lemon <jamie.lemon@dijipiji.com>
2019-04-19 17:11:47 +0100

    Swift: MainViewController table view uses CakeData to populate cells

--- swift/CakeList/CakeList/MainViewController.swift
+++ swift/CakeList/CakeList/MainViewController.swift
@@ -10,19 +10,55 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
+    var LIST_DATA:[Any] = []
+    
     /**
      *
      */
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let success:Bool = startDataFetch()
+        
+        if success {
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    /**
+     *
+     */
+    func startDataFetch() -> Bool {
+        
+        let data:Data? = CakeData.getData()
+        
+        if data == nil {
+            
+        } else {
+            let json:Any? = CakeData.parseData(data!)
+            
+            if json != nil {
+                
+                LIST_DATA = json! as! [Any]
+                
+                print("LIST_DATA=\(LIST_DATA)")
+            }
+        }
+        
+        return false
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+         return 1
     }
     
     /**
      *
      */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+         return LIST_DATA.count
     }
     
     /**
@@ -31,10 +67,25 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "CakeCell")
         
-        cell.imageView!.image = UIImage(named:"icon-cake")!
-        cell.textLabel!.text = "Title"
+        let object:[String:String] = LIST_DATA[indexPath.row] as! [String:String]
         
-        cell.detailTextLabel!.text = "Subtitle"
+        //(person as! [String:String])["name"]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["desc"]
+        
+        var image:UIImage!
+        
+        let url:URL! = URL(string:object["image"]!)
+
+        if let data:Data = try? Data.init(contentsOf: url) {
+            image = UIImage(data: data)
+        }
+        
+        if image == nil {
+            image = UIImage(named:"icon-cake")!
+        }
+        
+        cell.imageView!.image = image
         
         return cell
     }
